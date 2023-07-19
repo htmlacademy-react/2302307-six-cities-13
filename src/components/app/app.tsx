6ee8e2f0058a8bb005218bce482feef3182d9a67
anvts <worontsov.andrey@gmail.com>
2023-07-19 05:10:09 +0300

    Add routes to App component

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,4 +1,10 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import MainPage from '../../pages/main-page/main-page';
+import { AppRoute } from '../../const';
+import LoginPage from '../../pages/login-page/login-page';
+import FavoritesPage from '../../pages/favorites-page/favorites-page';
+import OfferPage from '../../pages/offer-page/offer-page';
+import Page404 from '../../pages/page-404/page-404';
 
 type AppProps = {
   placesCount: number;
@@ -6,7 +12,30 @@ type AppProps = {
 
 function App({placesCount}: AppProps): React.JSX.Element {
   return(
-    <MainPage placesCount={placesCount} />
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Main}
+          element={<MainPage placesCount={placesCount} />}
+        />
+        <Route
+          path={AppRoute.Login}
+          element={<LoginPage />}
+        />
+        <Route
+          path={AppRoute.Favorites}
+          element={<FavoritesPage />}
+        />
+        <Route
+          path={AppRoute.Offer}
+          element={<OfferPage />}
+        />
+        <Route
+          path='*'
+          element={<Page404 />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/const.ts
+++ src/const.ts
@@ -1,1 +1,8 @@
 export const PLACES_COUNT = 15;
+
+export enum AppRoute {
+  Main = '/',
+  Login = '/login',
+  Favorites = '/favorites',
+  Offer = '/offer'
+}
