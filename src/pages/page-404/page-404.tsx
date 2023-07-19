14138496b85c41234cfe9a092c72b46f222a4994
anvts <worontsov.andrey@gmail.com>
2023-07-19 04:27:51 +0300

    Create component for page 404

---
+++ src/pages/page-404/page-404.tsx
@@ -0,0 +1,12 @@
+
+
+function Page404(): React.JSX.Element {
+  return (
+    <>
+      <h1>404 Page not found</h1>
+      <a href="/">Go to main page</a>
+    </>
+  );
+}
+
+export default Page404;
